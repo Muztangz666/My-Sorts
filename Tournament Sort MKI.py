8918d43a433257bcc74098f3cbd4e988cf0ccfc8
Brandon Miles <63470185+Muztangz666@users.noreply.github.com>
2021-01-29 21:33:01 -0600

    Update Tournament Sort MKI.py
    Added more comments, better spacing, and a test case.

--- Tournament Sort MKI.py
+++ Tournament Sort MKI.py
@@ -11,6 +11,7 @@ def UpdateArray(ArrayToSort, LastIndex):
     # List to hold winners of each comparison.
     # Final size: ceiling(ArrayToSort.Length / 2).
     WinnersList = []
+
     # Adds the largest value of each comparison to WinnersList.
     # Run time: O(log n).
     for i in range(0, len(ArrayToSort), 2):
@@ -24,13 +25,15 @@ def UpdateArray(ArrayToSort, LastIndex):
       else:
         WinnersList.append(ArrayToSort[i])
       
-      # Exits the loop when all elements have been checked.
+      # Exits the loop if all elements have been checked.
       if((i + 1) == LastIndex):
         break
+
     # TournamentSort is called again until there is only one element
     # left in the array being passed-the largest value in the array.
     NewLastIndex = len(WinnersList) - 1
     return TournamentSort(WinnersList, NewLastIndex)
+  # End TournamentSort().
   
   MaxVal = TournamentSort(ArrayToSort, LastIndex)
   # Gets index of MaxVal from ArrayToSort.
@@ -40,6 +43,12 @@ def UpdateArray(ArrayToSort, LastIndex):
     if(ArrayToSort[i] == MaxVal):
       MaxIndex = i
       break
+
   # Moves the largest value to the end of the array.
   ArrayToSort[MaxIndex], ArrayToSort[LastIndex] = ArrayToSort[LastIndex], ArrayToSort[MaxIndex]
   UpdateArray(ArrayToSort, LastIndex - 1)
+  
+  # Test case.
+  TestArray = [1, 16, 7, 14, 12, 5, 3, 4, 8, 2, 11, 15, 6, 9, 10, 13, 1, 7, 15]
+  UpdateArray(TestArray, len(TestArray) - 1)
+  print("Final Array:", TestArray)
